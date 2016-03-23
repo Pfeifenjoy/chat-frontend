b0fa65071fb890172fac088d2f7075152fbf5f6c
Arwed Mett <arwed.mett@googlemail.com>
2016-03-23 17:31:17 +0100

    responsive login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,42 +11,43 @@ export default class Login extends Component {
 
     render() {
         return <div id="login_wrapper">
-            <section id="login">
-                <div className="row vertical-offset-100">
+            <section id="login" className="container">
+                <div className="row">
+                    <div className="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">Please sign in</h3>
+                            </div>
+                            <div className="panel-body">
+                                <form accept-charset="UTF-8" role="form" action="index.html#/app" method="post"
+                                      onSubmit={this.handleSubmit.bind(this)}>
+                                    <fieldset>
+                                        <div className="form-group">
+                                            <input className="form-control" placeholder="Username"
+                                                   value={this.state.username}
+                                                   onChange={this.handleUsernameChange.bind(this)} name="username"
+                                                   type="text"/>
+                                        </div>
+                                        <div className="form-group">
+                                            <input className="form-control" placeholder="Password"
+                                                   value={this.state.password}
+                                                   onChange={this.handlePasswordChange.bind(this)} name="password"
+                                                   type="password"
+                                                   value=""/>
+                                        </div>
+                                        <div className="checkbox">
+                                            <label>
+                                                <input name="remember" type="checkbox" value="Remember Me"> Remember
+                                                    Me </input>
+                                            </label>
+                                        </div>
+                                        <input className="btn btn-lg btn-success btn-block" type="submit" value="Login"/>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
 
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Please sign in</h3>
-                        </div>
-                        <div className="panel-body">
-                            <form accept-charset="UTF-8" role="form" action="index.html#/app" method="post"
-                                  onSubmit={this.handleSubmit.bind(this)}>
-                                <fieldset>
-                                    <div className="form-group">
-                                        <input className="form-control" placeholder="Username"
-                                               value={this.state.username}
-                                               onChange={this.handleUsernameChange.bind(this)} name="username"
-                                               type="text"/>
-                                    </div>
-                                    <div className="form-group">
-                                        <input className="form-control" placeholder="Password"
-                                               value={this.state.password}
-                                               onChange={this.handlePasswordChange.bind(this)} name="password"
-                                               type="password"
-                                               value=""/>
-                                    </div>
-                                    <div className="checkbox">
-                                        <label>
-                                            <input name="remember" type="checkbox" value="Remember Me"> Remember
-                                                Me </input>
-                                        </label>
-                                    </div>
-                                    <input className="btn btn-lg btn-success btn-block" type="submit" value="Login"/>
-                                </fieldset>
-                            </form>
-                        </div>
                     </div>
-
                 </div>
             </section>
         </div>
